f58d86d4ac03b15384f647293d3546ba35cc69c0
earnop <@Poiuy987>
2019-10-18 08:15:18 +0800

    修改redis配置

--- mol_common/src/main/java/util/TimeUtil.java
+++ mol_common/src/main/java/util/TimeUtil.java
@@ -16,7 +16,7 @@ public class TimeUtil {
     public static String payOrderFormat = "yyyyMMddHHmmssSSS";
 
     /**
-     * 把时间戳转换为日期时间格式
+     * 把时间戳转换为LocalDateTime类型的日期时间格式
      * @param timestamp
      * @return
      */
@@ -38,7 +38,7 @@ public class TimeUtil {
 
 
     /**
-     * 转换时间格式
+     * 把LocalDateTime格式的时间转换为String·类型的时间
      * @param ldt           需要转换的时间
      * @param formatType    想要转换成啥样
      * @return              转换好的时间字符串
@@ -49,6 +49,10 @@ public class TimeUtil {
     }
 
 
+    /**
+     * 获取当前年月日时分秒"yyyy-MM-dd HH:mm:ss"
+     * @return
+     */
     public static String getNow(){
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormaterDateTime);
@@ -62,18 +66,30 @@ public class TimeUtil {
     }
 
 
-
+    /**
+     *
+     * 获取当前年月日时分秒"yyyy-MM-dd HH:mm:ss"
+     * @return
+     */
     public static String getNowDateTime(){
         return getNow();
     }
 
+    /**
+     * 获取当前日期   XXXX-XX-XX
+     * @return
+     */
     public static String getNowOnlyDate(){
         LocalDateTime now = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormaterOnlyDate);
         return now.format(formatter);
     }
 
-    public static String get_last_year_date()//获取去年同月日期
+    /**
+     * 获取去年同月日期
+     * @return
+     */
+    public static String get_last_year_date()
     {
         SimpleDateFormat format = new SimpleDateFormat(dateFormaterOnlyDate);
         Calendar c = Calendar.getInstance();
@@ -83,5 +99,55 @@ public class TimeUtil {
          return format.format(y);
     }
 
+    /**
+     * 把字符串类型的日期时间转换为LocalDateTime类型
+     * @param timeStr               字符串型的日期、时间
+     * @param format                转换的格式
+     * @return
+     */
+    public static LocalDateTime getLocalDataTimeFromStr(String timeStr,String format){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
+        return LocalDateTime.parse(timeStr,dateTimeFormatter);
+    }
+
+
+
+
+    /**
+     * 通过输入指定日期时间生成cron表达式
+     * @param date
+     * @return cron表达式
+     */
+    public static String getCron(Date date) {
+        String dateFormat = "ss mm HH dd MM ? yyyy";
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+        String formatTimeStr = null;
+        if (date != null) {
+            formatTimeStr = sdf.format(date);
+        }
+        return formatTimeStr;
+    }
+
+
+    /**
+     * java.time.LocalDateTime   -->   java.util.Date
+     */
+    public static Date LocalDateTimeToDate(LocalDateTime localDateTime) {
+        ZoneId zone = ZoneId.systemDefault();
+        Instant instant = localDateTime.atZone(zone).toInstant();
+        return Date.from(instant);
+    }
+
+
+    /**
+     * java.util.Date --> java.time.LocalDateTime
+     */
+    public static LocalDateTime dateToLocalDateTime() {
+        java.util.Date date = new java.util.Date();
+        Instant instant = date.toInstant();
+        ZoneId zone = ZoneId.systemDefault();
+        LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, zone);
+        return localDateTime;
+    }
 
 }
