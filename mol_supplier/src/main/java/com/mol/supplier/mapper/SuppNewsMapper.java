9fe78d87dfca6cae877f806603736fdc8a7280d3
Administrator <1776080295@qq.com>
2019-11-15 16:02:00 +0800

    11.15号，第三方摩尔资讯over

---
+++ mol_supplier/src/main/java/com/mol/supplier/mapper/SuppNewsMapper.java
@@ -0,0 +1,7 @@
+package com.mol.supplier.mapper;
+
+import com.mol.base.BaseMapper;
+import com.mol.supplier.entity.thirdPlatform.SuppNews;
+
+public interface SuppNewsMapper extends BaseMapper<SuppNews> {
+}
