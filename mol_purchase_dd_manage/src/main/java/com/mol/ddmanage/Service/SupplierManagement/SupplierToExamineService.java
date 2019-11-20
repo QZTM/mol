f39c178ba4c7a5838849321da0cf3ae2d80df059
’1282140634‘ <414190342@163.com>
2019-11-20 15:50:49 +0800

    修改供应商审核模块

--- mol_purchase_dd_manage/src/main/java/com/mol/ddmanage/Service/SupplierManagement/SupplierToExamineService.java
+++ mol_purchase_dd_manage/src/main/java/com/mol/ddmanage/Service/SupplierManagement/SupplierToExamineService.java
@@ -11,9 +11,21 @@ public class SupplierToExamineService
 {
     @Resource
     SupplierToExamineMapper supplierToExamineMapper;
-    public String AgreeOrRefuseLogic(String pk_supplier,String supstate_single)
+    public String AgreeOrRefuseLogic(String pk_supplier,String process,String statu)
     {
-        supplierToExamineMapper.AgreeOrRefuse(pk_supplier,supstate_single, DataUtil.GetNowSytemTime());
+        if (statu.equals("1"))//基础供应商
+        {
+            supplierToExamineMapper.AgreeOrRefuse_normal(pk_supplier,process, DataUtil.GetNowSytemTime());
+        }
+        else if (statu.equals("2"))//战略供应商
+        {
+            supplierToExamineMapper.AgreeOrRefuse_strategy(pk_supplier,process, DataUtil.GetNowSytemTime());
+        }
+        else if (statu.equals("3"))//单一供应商
+        {
+            supplierToExamineMapper.AgreeOrRefuse_single(pk_supplier,process, DataUtil.GetNowSytemTime());
+        }
+
         return "提交成功";
     }
 }
